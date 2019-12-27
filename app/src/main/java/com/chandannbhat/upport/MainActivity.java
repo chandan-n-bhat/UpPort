49427be98fb5349ab0a153f1d196c98c6af1dbd1
chandan-n-bhat <40853868+chandan-n-bhat@users.noreply.github.com>
2019-12-27 22:56:57 +0530

    Dynamic url added

--- app/src/main/java/com/chandannbhat/upport/MainActivity.java
+++ app/src/main/java/com/chandannbhat/upport/MainActivity.java
@@ -2,11 +2,14 @@ package com.chandannbhat.upport;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,10 +22,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         webView1 = (WebView)findViewById(R.id.wv_1);
-        url = "https://www.creative-tim.com/";
+        //url = "https://www.creative-tim.com/";
+
+        Intent intent = getIntent();
+        url = intent.getStringExtra("web_url");
+//        Toast.makeText(this, url, Toast.LENGTH_SHORT).show();
 
         webView1.getSettings().setJavaScriptEnabled(true);
-        webView1.loadUrl("https://www.creative-tim.com/");
+        webView1.loadUrl(url);
 
         webView1.setWebViewClient(new WebViewClient());
     }
